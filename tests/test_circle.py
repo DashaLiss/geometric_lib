1db51139a2f41cbd2535b90cc25c5916062cda9e
Daria Ivanova <129103473+DashaLiss@users.noreply.github.com>
2024-11-23 16:25:02 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -5,34 +5,29 @@ import calculate
 class TestCircle(unittest.TestCase):
 
     def test_area_positive(self):
-        res_area_one_positive = calculate.calc("circle", "area", [1])
-        self.assertEqual(res_area_one_positive, 3.141592653589793)
-
-        res_area_two_positive = calculate.calc("circle", "area", [0])
-        self.assertEqual(res_area_two_positive, 0.0)
+        self.assertEqual(calculate.calc("circle", "area", [1]),
+                         3.141592653589793)
+        self.assertEqual(calculate.calc("circle", "area", [0]),
+                         0.0)
 
     def test_area_negative(self):
+        error_message = "incorrect data"
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "area", [-666])
-        self.assertEqual(str(context.exception),
-                         "incorrect data")
+        self.assertEqual(str(context.exception), error_message)
 
     def test_perimeter_positive(self):
-        res_perimeter_one_positive = calculate.calc(
-            "circle", "perimeter", [666])
-        self.assertEqual(res_perimeter_one_positive, 
-                         4184.601414581604)
-
-        res_perimeter_two_positive = calculate.calc(
-            "circle", "perimeter", [0])
-        self.assertEqual(res_perimeter_two_positive,
-                        0.0)
+        self.assertEqual(calculate.calc("circle", "perimeter", [1]),
+                         6.283185307179586)
+        self.assertEqual(calculate.calc("circle", "perimeter", [0]),
+                         0.0)
 
     def test_perimeter_negative(self):
+        error_message = "incorrect data"
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "perimeter", [-666])
-        self.assertEqual(str(context.exception),
-                         "incorrect data")
+        self.assertEqual(str(context.exception), error_message)
+
 
 if __name__ == "__main__":
     unittest.main()
