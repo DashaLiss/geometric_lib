645192d9d442729ea57a80b398d4f650e7ad6599
Dasha Ivanova <DashaLiss1@yandex.ru>
2024-11-23 14:01:27 +0300

    calculate circle suare triangle was changed

--- calculate.py
+++ calculate.py
@@ -1,17 +1,27 @@
-import circle
-import square
+import circle  # noqa: F401
+import square  # noqa: F401
+import triangle  # noqa: F401
 
 
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+	"area-circle": 1,
+    "perimeter-circle": 1,
+    "area-square": 1,
+    "perimeter-square": 1,
+    "area-triangle": 3,
+    "perimeter-triangle": 3
+	}
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
-
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    assert fig in figs, f"Invalid figure: {fig}. Allowed values are {figs}."
+    assert func in funcs, "Invalid function: {func}. Allowed values are \
+    {funcs}."
+    assert len(size) == sizes.get(f"{func}-{fig}", 1), \
+        f"Invalid number of parameters for {func} of {fig}."
+    result = eval(f'{fig}.{func}(*{size})')
+    return result
 
 if __name__ == "__main__":
 	func = ''
@@ -25,9 +35,7 @@ if __name__ == "__main__":
 		func = input(f"Enter function name, avaliable are {funcs}:\n")
 	
 	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+		size = list(map(int, input("Input figure sizes"
+							"separated by space\n").split(' ')))
 	
-	calc(fig, func, size)
-
-
-
+	print(calc(fig, func, size))
