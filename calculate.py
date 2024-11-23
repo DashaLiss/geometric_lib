d1ac4a1d2f3b41556e850101cbb7efd308ad84a1
Daria Ivanova <129103473+DashaLiss@users.noreply.github.com>
2024-11-23 15:45:32 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -2,17 +2,17 @@ import circle  # noqa: F401
 import square  # noqa: F401
 import triangle  # noqa: F401
 
-
 figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 sizes = {
-	"area-circle": 1,
+    "area-circle": 1,
     "perimeter-circle": 1,
     "area-square": 1,
     "perimeter-square": 1,
     "area-triangle": 3,
     "perimeter-triangle": 3
-	}
+}
+
 
 def calc(fig, func, size):
     assert fig in figs, f"Invalid figure: {fig}. Allowed values are {figs}."
@@ -23,19 +23,20 @@ def calc(fig, func, size):
     result = eval(f'{fig}.{func}(*{size})')
     return result
 
+
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes"
-							"separated by space\n").split(' ')))
-	
-	print(calc(fig, func, size))
+    func = ''
+    fig = ''
+    size = list()
+
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}: \n")
+
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}: \n")
+
+    while len(size) != sizes.get(f"{func}-{fig}", 1):
+        size = list(map(int, input("Input figure sizes"
+                                   "separated by space.\n").split(' ')))
+
+    print(calc(fig, func, size))
